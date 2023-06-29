2aa8487c465c0789603088bee2ee46c1ea3b3478
AnilNITT <patidaranil0791@gmail.com>
2023-06-29 16:30:34 +0530

    update

--- server.js
+++ server.js
@@ -152,8 +152,8 @@ socketIO.on("connection", async (socket) => {
 
   let updateReceived = await MessageModal.updateMany(
     { receiverId: socket.decoded.id, seenStatus: "send" },
-    { $set: { seenStatus: "received"} }
-    // { seenStatus: "received" }
+    // { $set: { seenStatus: "received"} }
+    { seenStatus: "received" }
   );
 
 
