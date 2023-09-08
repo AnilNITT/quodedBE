53050f49d41df540d03a0e3834702378c7e2612e
Anil Patidar <patidaranil0791@gmail.com>
2023-09-08 18:03:24 +0530

    Update cloudController.js

--- Module/AWS/controller/cloudController.js
+++ Module/AWS/controller/cloudController.js
@@ -17,8 +17,8 @@ var bucket = "quoded-cloud-data";
 const s3Client = new S3Client({
   region: "us-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: "",
+    secretAccessKey: "",
   },
 });
 
